59c33efdb0abefdbca4c0320e1b906aaea6d5034
csrgxtu <1246506786@qq.com>
2015-04-22 23:17:21 +0800

    Gps Location worked, can get gps data now

---
+++ GpsLocatoin/src/io/github/csrgxtu/GpsLocation/MainActivity.java
@@ -0,0 +1,67 @@
+package io.github.csrgxtu.GpsLocation;
+
+import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+public class MainActivity extends ActionBarActivity {
+	Button btnShowLocation;
+	GPSTracker gps;
+	
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+		
+		btnShowLocation = (Button) findViewById(R.id.btnShowLocation);
+        
+        // show location button click event
+        btnShowLocation.setOnClickListener(new View.OnClickListener() {
+             
+            @Override
+            public void onClick(View arg0) {        
+                // create class object
+                gps = new GPSTracker(MainActivity.this);
+ 
+                // check if GPS enabled     
+                if(gps.canGetLocation()){
+                     
+                    double latitude = gps.getLatitude();
+                    double longitude = gps.getLongitude();
+                     
+                    // \n is for new line
+                    Toast.makeText(getApplicationContext(), "Your Location is - \nLat: " + latitude + "\nLong: " + longitude, Toast.LENGTH_LONG).show();    
+                }else{
+                    // can't get location
+                    // GPS or Network is not enabled
+                    // Ask user to enable GPS/network in settings
+                    gps.showSettingsAlert();
+                }
+                 
+            }
+        });
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.main, menu);
+		return true;
+	}
+
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		// Handle action bar item clicks here. The action bar will
+		// automatically handle clicks on the Home/Up button, so long
+		// as you specify a parent activity in AndroidManifest.xml.
+		int id = item.getItemId();
+		if (id == R.id.action_settings) {
+			return true;
+		}
+		return super.onOptionsItemSelected(item);
+	}
+}
