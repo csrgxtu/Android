de39e0368218567dea32fbbd6b40da887a16c86d
csrgxtu <1246506786@qq.com>
2015-04-23 22:47:22 +0800

    add RESTful API Server

--- Server/index.js
+++ Server/index.js
@@ -2,15 +2,25 @@ var express = require('express');
 
 var app = express();
 
+var data = {
+  error: 0,
+  msg: 'Success',
+  recs: 'object',
+};
+
+app.put('/GPS', function cb(req, res) {
+  return res.json(data);
+});
+
 app.get('/GPS', function cb(req, res) {
-  console.log("INFO: client connected");
+  return res.json(data);
+});
 
-  var data = {
-    name: 'archer',
-    sex: 'male',
-    rich: false
-  };
+app.post('/GPS', function cb(req, res) {
+  return res.json(data);
+});
 
+app.delete('/GPS', function cb(req, res) {
   return res.json(data);
 });
 
