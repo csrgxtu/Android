7501ff0830981dc90d7dd771b1c9aaa550369b50
csrgxtu <1246506786@qq.com>
2015-04-25 08:53:46 +0800

    temp commit

--- AndroidService/src/io/github/csrgxtu/AndroidService/RESTClient.java
+++ AndroidService/src/io/github/csrgxtu/AndroidService/RESTClient.java
@@ -1,44 +1,16 @@
 package io.github.csrgxtu.AndroidService;
 
-import org.json.simple.JSONObject;
-
-import android.util.Log;
-
-import com.mashape.unirest.http.HttpResponse;
-import com.mashape.unirest.http.JsonNode;
-import com.mashape.unirest.http.Unirest;
-import com.mashape.unirest.http.exceptions.UnirestException;
+import org.apache.http.client.HttpClient;
 
 public class RESTClient {
-	private String TAG = "RESTClient";
 	private String Url;
 	
 	public RESTClient(String url) {
 		this.Url = url;
 	}
 	
-	public boolean doRequest() {
-		try {
-			//Unirest.setTimeouts(long connectionTimeout, long socketTimeout);
-			//HttpResponse<JsonNode> jsonResponse = Unirest.get(this.Url)
-			//		.asJson();
-			
-			JSONObject obj = new JSONObject();
-			obj.put("lat", 12.3456);
-			obj.put("lon", 121.2389);
-			obj.put("time", "2015-04-23 13:30:00");
-			
-			HttpResponse<JsonNode> jsonResponse = Unirest.post(this.Url)
-				.queryString("apiKey", obj)
-				.asJson();
-			
-			Log.i(TAG, jsonResponse.getBody().getObject().toString());
-		} catch (UnirestException e) {
-			// TODO Auto-generated catch block
-			//e.printStackTrace();
-			return false;
-		}
-		
+	public boolean doGet() {
+		HttpClient client = new HttpClient();
 		return true;
 	}
 	
--- AndroidService/src/io/github/csrgxtu/AndroidService/TestService.java
+++ AndroidService/src/io/github/csrgxtu/AndroidService/TestService.java
@@ -1,5 +1,6 @@
 package io.github.csrgxtu.AndroidService;
 
+//import io.github.csrgxtu.HttpClientTut.RESTClient;
 import android.app.IntentService;
 import android.content.Intent;
 import android.util.Log;
@@ -14,9 +15,9 @@ public class TestService extends IntentService {
 	@Override
 	protected void onHandleIntent(Intent intent) {
 		Log.i(TAG, "In IntentService");
-		String url = "http://192.168.10.176:3000/GPS";
+		String url = "http://192.168.10.210:3000/GPS";
 		RESTClient client = new RESTClient(url);
-		client.doRequest();
+		Log.i(TAG, client.doGet());
 		Log.i(TAG, "After Http REST Request");
 	}
 }
