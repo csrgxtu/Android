1ba7c5dae095d0148c5fc5234e88fe25f4148c63
archer <1246506786@qq.com>
2015-11-05 08:35:47 -0500

    GoogleHelloWorld

--- LoginDemo/app/src/main/java/io/github/csrgxtu/logindemo/LoginActivity.java
+++ LoginDemo/app/src/main/java/io/github/csrgxtu/logindemo/LoginActivity.java
@@ -3,6 +3,7 @@ package io.github.csrgxtu.logindemo;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.design.widget.TextInputLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
@@ -15,16 +16,17 @@ public class LoginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
-//        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-//        setSupportActionBar(toolbar);
+        final TextInputLayout usernameWrapper = (TextInputLayout) findViewById(R.id.usernameWrapper);
+        final TextInputLayout passwordWrapper = (TextInputLayout) findViewById(R.id.passwordWrapper);
+        usernameWrapper.setHint("Username");
+        passwordWrapper.setHint("Password");
 
-//        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-//        fab.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View view) {
-//                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-//                        .setAction("Action", null).show();
-//            }
-//        });
+        
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // STUB
+            }
+        });
     }
 }
