276995c82c3294b99158ebe68053f6e1813bbe85
archer <1246506786@qq.com>
2015-11-26 21:17:09 +0800

    trying to put the restful client code into another file

--- AndroidRest/app/src/main/java/io/github/csrgxtu/androidrest/MainActivity.java
+++ AndroidRest/app/src/main/java/io/github/csrgxtu/androidrest/MainActivity.java
@@ -10,7 +10,13 @@ import com.squareup.okhttp.Callback;
 import com.squareup.okhttp.OkHttpClient;
 import com.squareup.okhttp.Request;
 import com.squareup.okhttp.Response;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
 import java.io.IOException;
+
+import io.github.csrgxtu.androidrest.RestfulClient;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -22,18 +28,24 @@ public class MainActivity extends AppCompatActivity {
 //        String url = "https://raw.github.com/square/okhttp/master/README.md";
         String url = "http://192.168.1.37:8000/gps/0/10";
         try {
-            doReq(url, new Callback() {
+            RestfulClient restClient = new RestfulClient();
+            restClient.doGet(url, new Callback() {
                 @Override
                 public void onFailure(Request request, IOException e) {
                     Log.i("onCreate", "request failure");
                 }
 
-                @Override public void onResponse(Response response) throws IOException {
+                @Override
+                public void onResponse(Response response) throws IOException {
                     if (response.isSuccessful()) {
                         String responseStr = response.body().string();
+                        try {
+                            JSONObject Jobject = new JSONObject(responseStr);
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
                         // Do what you want to do with the response.
                         Log.i("onCreate", "response successful");
-                        Log.i("onCreate", responseStr);
                     } else {
                         // Request not successful
                         Log.i("onCreate", "response is not successful");
@@ -45,23 +57,13 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
-    private String run(String url) throws IOException {
-        OkHttpClient client = new OkHttpClient();
-        Request request = new Request.Builder()
-                .url(url)
-                .build();
-
-        Response response = client.newCall(request).execute();
-        return response.body().string();
-    }
-
-    private Call doReq(String url, Callback callback) throws IOException {
-        OkHttpClient client = new OkHttpClient();
-        Request request = new Request.Builder()
-                .url(url)
-                .build();
-        Call call = client.newCall(request);
-        call.enqueue(callback);
-        return call;
-    }
+//    private String run(String url) throws IOException {
+//        OkHttpClient client = new OkHttpClient();
+//        Request request = new Request.Builder()
+//                .url(url)
+//                .build();
+//
+//        Response response = client.newCall(request).execute();
+//        return response.body().string();
+//    }
 }
